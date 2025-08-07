c8364121affecef9e20c7d3a4ef91eb80c7ccc57
preetham4693 <npreetham993@gmail.com>
2025-08-07 18:17:12 +0530

    added autoplay

--- rps/rps.js
+++ rps/rps.js
@@ -1,83 +1,102 @@
+
 let score = JSON.parse(localStorage.getItem('score'));
 if (score === null) {
   score = {
     wins: 0,
     lose: 0,
-    ties: 0
+    ties: 0,
   };
-} 
-
-updateScore();
-
-
-function playGame(playerMove){
-var computerMove = pickComputerMove();
-
-var result = '';
-
-if (playerMove==='scissors'){
-    if (computerMove === 'rock') {
-    result = 'You lose!';
-  } else if (computerMove === 'paper') {
-    result = 'You won';
-  } else if (computerMove === 'scissors') {
-    result = 'Tie';
-  }
-}else if(playerMove==='paper'){
-    if (computerMove === 'rock') {
-    result = 'You won';
-  } else if (computerMove === 'paper') {
-    result = 'Tie';
-  } else if (computerMove === 'scissors') {
-    result = 'You lose!';
-  }
-}else if (playerMove==='rock'){
-    if (computerMove === 'rock') {
-    result = 'Tie';
-  } else if (computerMove === 'paper') {
-    result = 'You lose!';
-  } else if (computerMove === 'scissors') {
-    result = 'You won';
-  }
 }
 
-if (result==='You won'){
-    score.wins+=1;
-  }else if (result==='You lose!'){
-    score.lose+=1;
-  }else if(result==='Tie'){
-    score.ties+=1;
- }
-localStorage.setItem('score',JSON.stringify(score));
+let isAutoPlaying = false;
+let intervalId;
 
-updateScore();
-
-document.querySelector('.js-moves')
-    .innerHTML=`You 
-    <img src="rps-images/${playerMove}-emoji.png" class="move-img">
-    <img src="rps-images/${computerMove}-emoji.png" class="move-img">
-    Computer`;
-
-
-document.querySelector('.js-results')
-    .innerHTML= result;
-
+// Update score display after DOM loaded       Use window.onload to ensure your JavaScript code runs only after the whole page is loaded
+window.onload = function () {
+  updateScore();
 };
 
+function autoplay() {
+  const btn = document.getElementById('autoplay-btn');
+  if (!isAutoPlaying) {
+    intervalId = setInterval(function () {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
 
-
-function updateScore(){
-  document.querySelector('.js-score')
-    .innerHTML = ` wins: ${score.wins},lose:${score.lose},tie:${score.ties}`
+    isAutoPlaying = true;
+    btn.textContent = 'Stop Auto';
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    btn.textContent = 'Auto play';
+  }
 }
 
-function pickComputerMove(){
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose!';
+    } else if (computerMove === 'paper') {
+      result = 'You won';
+    } else {
+      result = 'Tie';
+    }
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You won';
+    } else if (computerMove === 'paper') {
+      result = 'Tie';
+    } else {
+      result = 'You lose!';
+    }
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie';
+    } else if (computerMove === 'paper') {
+      result = 'You lose!';
+    } else {
+      result = 'You won';
+    }
+  }
+
+  if (result === 'You won') {
+    score.wins += 1;
+  } else if (result === 'You lose!') {
+    score.lose += 1;
+  } else if (result === 'Tie') {
+    score.ties += 1;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+  updateScore();
+
+  document.querySelector('.js-moves').innerHTML = `You
+    <img src="rps-images/${playerMove}-emoji.png" class="move-img" alt="${playerMove}" />
+    <img src="rps-images/${computerMove}-emoji.png" class="move-img" alt="${computerMove}" />
+    Computer`;
+
+  document.querySelector('.js-results').innerHTML = result;
+}
+
+function updateScore() {
+  const scoreElem = document.querySelector('.js-score');
+  if (scoreElem) {
+    scoreElem.innerHTML = `Wins: ${score.wins}, Lose: ${score.lose}, Ties: ${score.ties}`;
+  }
+}
+
+function pickComputerMove() {
   const randomNum = Math.random();
   let computerMove = '';
 
-  if (randomNum >= 0 && randomNum < 1/3) {
+  if (randomNum >= 0 && randomNum < 1 / 3) {
     computerMove = 'rock';
-  } else if (randomNum >= 1/3 && randomNum < 2/3) {
+  } else if (randomNum >= 1 / 3 && randomNum < 2 / 3) {
     computerMove = 'paper';
   } else {
     computerMove = 'scissors';
